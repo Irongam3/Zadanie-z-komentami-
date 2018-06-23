cb46ad013778d6967e74d6ee298616627bb42360
Piotr Bielecki <hfx.music@gmail.com>
2018-06-24 01:15:30 +0200

    Added comments to scripts.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,7 +1,7 @@
-// JavaScript Document
-{
+// To jest pierwszy comment - trwa on do konca linii.
+
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
+alert('Hello, ' + name);//Tu pojawia się alert
 console.log('Hello, ' + name);
 console.log(name);
-}
+/* możemy też zrobić to tak */
